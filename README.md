be3a1dd8b495543b1d8bb71079241da95a16872e
Ricky Nguyen <rnguyen89@gmail.com>
2018-09-11 15:58:35 -0700

    added live app to readme

--- README.md
+++ README.md
@@ -2,19 +2,22 @@
 
 Google like search engine for Reddit.
 
+Live app: https://quirky-benz-fce7cb.netlify.com/
+
 # Introduction
 
 Just a mini project to learn more about using APIs as well as practicing with React.
 
-
 # Technology
 
 ## Frontend
+
 - React
 
 ## Backend
+
 - tba
 
 ## Security
-- tba
 
+- tba
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
-import About from './About';
-import NotFound from './NotFound';
+import About from "./About";
+import NotFound from "./NotFound";
 
-export default () =>
+export default () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/home" component={Home} />
     <Route exact path="/about" component={About} />
     <Route component={NotFound} />
   </Switch>
+);
