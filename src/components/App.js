aed6fecbd4e741c5c7fcb8a35b9aaee377227ff5
Ricky Nguyen <rnguyen89@gmail.com>
2018-09-11 16:02:42 -0700

    commented out thread section

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 
 import Routes from "./Routes";
-import Form from './Form';
-import SearchResults from './SearchResults';
-import Footer from './Footer';
+import Form from "./Form";
+import SearchResults from "./SearchResults";
+import Footer from "./Footer";
 
-import './App.css';
-
-
+import "./App.css";
 
 // const API_KEY = 'k7D9kBkFB-oSQlLXIrAdzNzuWI8';
 
-
 class App extends Component {
-
   state = {
     redditResults: []
-  }
+  };
 
-  getRedditData = async (e) => {
+  getRedditData = async e => {
     e.preventDefault();
     const searchTerm = e.target.elements.searchTerm.value;
     const sortBy = e.target.elements.sortBy.value;
     const searchLimit = e.target.elements.searchLimit.value;
 
-    const api_call = await fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`);
+    const api_call = await fetch(
+      `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+    );
 
     const results = await api_call.json();
     this.setState({
       redditResults: results.data.children
     });
     console.log(this.state.redditResults);
-
-  }
+  };
 
   render() {
     return (
@@ -52,17 +49,15 @@ class App extends Component {
               <LinkContainer to="/about">
                 <NavItem>About</NavItem>
               </LinkContainer>
-              <LinkContainer to="thread">
+              {/* <LinkContainer to="thread">
                 <NavItem>Thread</NavItem>
-              </LinkContainer>
+              </LinkContainer> */}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
         <Routes />
         <Form getRedditData={this.getRedditData} />
-        <SearchResults
-          redditResults={this.state.redditResults}
-        />
+        <SearchResults redditResults={this.state.redditResults} />
         <Footer />
       </div>
     );
@@ -70,4 +65,3 @@ class App extends Component {
 }
 
 export default App;
-
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -9,6 +9,7 @@ export default () => (
     <Route exact path="/" component={Home} />
     <Route exact path="/home" component={Home} />
     <Route exact path="/about" component={About} />
+    {/* <Route exact path="/thread" component={thread} /> */}
     <Route component={NotFound} />
   </Switch>
 );
