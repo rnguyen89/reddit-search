272841de677c23355a3d087edb697521189f0b86
Ricky Nguyen <rnguyen89@gmail.com>
2018-09-11 16:07:09 -0700

    added info to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
 
-import './About.css';
-
+import "./About.css";
 
 const About = () => (
   <div>
     <Jumbotron>
       <h1>About Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque magnam, alias labore quam accusamus deleniti asperiores commodi adipisci eos pariatur debitis! Aperiam dolor eveniet sit optio at? Hic ipsa consequatur beatae maiores reiciendis reprehenderit debitis quo ducimus illo necessitatibus natus officia dolores ab, consequuntur maxime et repellendus, illum vero dolore?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quos eligendi velit eum natus ea veniam ipsam incidunt iste perspiciatis praesentium facilis, saepe quae delectus.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis quisquam fugiat maxime aut modi.</p>
+      <p>
+        A simple Reddit search app inspired by Google. Enter any keywords and
+        select the filters to your liking and see Reddit threads appear below.
+      </p>
+      <p>The app is created using React and Bootstrap.</p>
     </Jumbotron>
   </div>
-)
+);
 
 export default About;
