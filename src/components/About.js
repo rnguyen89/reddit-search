336ccb98499018592ea9dc5cab516e4a22419a86
Ricky Nguyen <rnguyen89@gmail.com>
2019-04-11 18:27:08 -0700

    changed color of footer and adding padding to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import { Jumbotron } from "react-bootstrap";
 import "./About.css";
 
 const About = () => (
-  <div>
-    <Jumbotron>
+  <Jumbotron>
+    <div className="about">
       <h1>About Page</h1>
       <p>
         A simple Reddit search app inspired by Google. Enter any keywords and
         select the filters to your liking and see Reddit threads appear below.
       </p>
       <p>The app is created using React and Bootstrap.</p>
-    </Jumbotron>
-  </div>
+    </div>
+  </Jumbotron>
 );
 
 export default About;
